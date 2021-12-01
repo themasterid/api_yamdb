c933c5c2725a368fa6bc46a2f055e9d232ed502c
Dmitriy <77408415+themasterid@users.noreply.github.com>
2021-12-01 10:23:23 +0500

    Update views.py

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,15 +1,17 @@
-from rest_framework import status, viewsets
+from rest_framework import mixins, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
+from rest_framework.mixins import (CreateModelMixin, DestroyModelMixin,
+                                   ListModelMixin)
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from reviews.models import Category, Genre, Review, Title, User
+
 from .permissions import AdminOnly
-from .serializers import  NotAdminSerializer, UsersSerializer
-from .serializers import UserSerializer
-from rest_framework import mixins, permissions, viewsets
-from reviews.models import User
+from .serializers import NotAdminSerializer, UsersSerializer
 
 
 class ModelMixinSet(CreateModelMixin, ListModelMixin,
