fc7091716dcd58d5cd77fd8ec5558d1f85767894
Dmitriy <77408415+themasterid@users.noreply.github.com>
2021-12-01 18:31:06 +0500

    update serializers.py 01.12.2021

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -59,10 +59,47 @@ class TitleWriteSerializer(serializers.ModelSerializer):
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    """Дмитрий!"""
-    pass
+    title = serializers.SlugRelatedField(
+        slug_field='name',
+        read_only=True
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    def validate_score(self, value):
+        if 0 > value > 10:
+            raise serializers.ValidationError('Оценка по 10-бальной шкале!')
+        return value
+
+    def validate(self, data):
+        request = self.context['request']
+        author = request.user
+        title_id = self.context.get('view').kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        if (
+            request.method == 'POST'
+            and Review.objects.filter(title=title, author=author).exists()
+        ):
+            raise ValidationError('Может существовать только один отзыв!')
+        return data
+
+    class Meta:
+        fields = '__all__'
+        model = Review
 
 
 class CommentsSerializer(serializers.ModelSerializer):
-    """Дмитрий!"""
-    pass
+    review = serializers.SlugRelatedField(
+        slug_field='text',
+        read_only=True
+    )
+    author = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comments
