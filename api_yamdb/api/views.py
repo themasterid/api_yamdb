3ecefa9d075ae543e8bc1cbe6e34e676d3de77a1
Klepikov Dmitriy <thebrootos@gmail.com>
2021-12-01 10:27:28 +0500

    fix mixins

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -6,6 +6,7 @@ from rest_framework.mixins import (CreateModelMixin, DestroyModelMixin,
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.views import APIView
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from reviews.models import Category, Genre, Review, Title, User
 
